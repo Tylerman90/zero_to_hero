758584c92223ccf3a4762d13ad0001e812a0eefe
Tyler Gunter <tyler.james.gunter@gmail.com>
2018-04-04 22:42:36 -0400

    definitions

---
+++ assessment1.py
@@ -0,0 +1,40 @@
+#write a brief description of all the
+#following Object Types and Data Structures
+#we've learned about:
+
+#Numbers: integers in python that can be
+#used in math, variable names, strings
+#or to count.
+
+#Strings: words, numbers or symbols that
+#are stored between quotes that are typically
+#used as output for the user to read, or for
+#the programmer to debug. Often used in
+#conjunction with print statements.
+
+#Lists: are sets of values that can be stored
+#and called upon to store and analyze information.
+
+#Tuples: sets of  keys and corresponding 
+#values that can be stored. However, these 
+#values cannot be changed once stored; they
+#are immutable.
+
+#Dictionaries: sets of keys and values that
+#can be stored and called upon. These keys
+#and values are mutable.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
