9832cb96e3435e23919d0b5bc10bbf98b2f0f044
Tyler Gunter <tyler.james.gunter@gmail.com>
2018-04-06 22:05:29 -0400

    sorted list

--- assessment1.py
+++ assessment1.py
@@ -33,14 +33,69 @@ x = (((((5 * 4) / 2) ** 2) + .50) - .25)
 print (x)
 
 a = 4 * (6 + 5)
-44
+#44
 print (a)
 b = 4 * 6 + 5
-29
+#29
 print (b)
 c = 4 + 6 * 5
-34
+#34
 print(c)
+
+#What is the type of the result of the 
+#expression 3 + 1.5 + 4?
+# = floating integer
+
+#to find square root you use
+# 2 ** (0.5)
+#to find square you use
+# 2 ** 2
+
+#STRINGS
+#Given the string 'hello' give an index 
+#command that returns 'e'.
+s = 'hello'
+print(s[1])
+
+#Reverse the string 'hello' using slicing:
+print(s[::-1])
+
+#Given the string hello, give two methods
+#of producing the letter 'o' using indexing.
+print(s[-1])
+print(s[4])
+
+
+#LISTS
+#Build this list [0,0,0] two separate ways.
+my_list = [0,0,0]
+print(my_list)
+a_list = [0,0]
+a_list.append(0)
+print(a_list)
+
+#Reassign 'hello' in this nested list to 
+#say 'goodbye' instead:
+list3 = [1,2,[3,4,'hello']]
+list3[2][2] = 'goodbye'
+print(list3)
+
+#Sort the list below:
+list4 = [5,3,4,6,1]
+list4.sort()
+print(list4)
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
