70bf13d905110b9068908e5a8a0dbc890f245b58
Tyler Gunter <tyler.james.gunter@gmail.com>
2018-05-22 21:17:53 -0400

    lesser_of_two_evens function

---
+++ funcpractice.py
@@ -0,0 +1,13 @@
+def lesser_of_two_evens(a,b):
+	if a%2 == 0 and b%2 == 0:
+		if a>b:
+			return b
+		else:
+			return a
+	elif a%3 == 0 or b%3 == 0:
+		if a>b:
+			return a
+		else:
+			return b
+
+print(lesser_of_two_evens(2,4))
