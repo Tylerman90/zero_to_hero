c552de7025c51d1c1075790448d537f567bff1ee
Tyler Gunter <tyler.james.gunter@gmail.com>
2018-05-24 10:50:46 -0400

    makes_twenty_function

--- funcpractice.py
+++ funcpractice.py
@@ -22,3 +22,10 @@ def animal_crackers(words):
 
 print(animal_crackers('Crazy Kangaroo'))
 
+def makes_twenty(n1,n2):
+	if n1 + n2 == 20 or n1 == 20 or n2 == 20:
+		return True
+	else:
+		return False
+
+print(makes_twenty(2,3))
