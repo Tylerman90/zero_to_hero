ecbaeba4f42b259c2301a683ab56686a60fad207
Tyler Gunter <tyler.james.gunter@gmail.com>
2018-05-24 10:42:17 -0400

    added animal crackers function

--- funcpractice.py
+++ funcpractice.py
@@ -1,17 +1,24 @@
 def lesser_of_two_evens(a,b):
 	if a%2 == 0 and b%2 == 0:
 		if a > b:
-			print(b)
+			return(b)
 		else:
-			print(a)
+			return(a)
 	elif a%3 == 0 or b%3 == 0:
 		if a > b:
-			print(a)
+			return(a)
 		else:
-			print(b)
+			return(b)
 	else:
 		print("Come on! Why do you have to make everything so complicated!")
 
-lesser_of_two_evens(4,2)
 
-def animal_crackers
+def animal_crackers(words):
+	two_words = words.split()
+	if two_words[0][0].lower() == two_words[1][0].lower():
+		return True
+	else:
+		return False
+
+print(animal_crackers('Crazy Kangaroo'))
+
