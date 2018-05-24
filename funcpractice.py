d1b04a6936b962f5ccc888389c690c73cfa2b89b
Tyler Gunter <tyler.james.gunter@gmail.com>
2018-05-23 21:45:49 -0400

    lesser_of_two_evens

--- funcpractice.py
+++ funcpractice.py
@@ -1,13 +1,17 @@
 def lesser_of_two_evens(a,b):
 	if a%2 == 0 and b%2 == 0:
-		if a>b:
-			return b
+		if a > b:
+			print(b)
 		else:
-			return a
+			print(a)
 	elif a%3 == 0 or b%3 == 0:
-		if a>b:
-			return a
+		if a > b:
+			print(a)
 		else:
-			return b
+			print(b)
+	else:
+		print("Come on! Why do you have to make everything so complicated!")
 
-print(lesser_of_two_evens(2,4))
+lesser_of_two_evens(4,2)
+
+def animal_crackers
