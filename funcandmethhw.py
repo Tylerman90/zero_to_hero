e896b0c275196a6c2458864b70ec3a6ca064fb8f
Tyler Gunter <tyler.james.gunter@gmail.com>
2018-06-03 21:00:57 -0400

    up_low function

--- funcandmethhw.py
+++ funcandmethhw.py
@@ -12,3 +12,18 @@ def ran_check(num,low,high):
 		return f"{num} is not in the range between {low} and {high}."
 
 print(ran_check(5,2,4))
+
+def up_low(s):
+	u = 0
+	l = 0
+	for letter in s:
+		if letter.isupper():
+			u += 1
+		elif letter.islower():
+			l += 1
+	print("Original String: ", s)
+	print(f'No. of Upper case Characters : {u}')
+	print(f'No. of Lower case Characters : {l}')
+
+up_low('Hello People')
+ 
