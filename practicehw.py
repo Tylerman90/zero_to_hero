db32e15b5de93cbbd7e15b6fe94546fe7423f624
Tyler Gunter <tyler.james.gunter@gmail.com>
2018-06-12 14:58:17 -0400

    ispangram function

--- practicehw.py
+++ practicehw.py
@@ -5,3 +5,41 @@ def multiply(numbers):
 	return total
 
 print(multiply([1,2,3,-4]))
+
+
+def palindrome(s):
+	for letters in s:
+		if s[0::] == s[::-1]:
+			return True
+		else:
+			return False
+
+#print(palindrome('helleh'))
+
+
+import string
+
+def ispangram(strl, alphabet=string.ascii_lowercase):
+	for letter in alphabet:
+		if letter not in strl:
+			return False
+		else:
+			return True
+
+print(ispangram("The quick brown fox jumps over the lazy dog"))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
