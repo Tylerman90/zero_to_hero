e3397fd54023c5983e9649e1071ee0bb4a983969
Tyler Gunter <tyler.james.gunter@gmail.com>
2018-07-15 12:01:48 -0400

    distance and slope defined

--- OOP.py
+++ OOP.py
@@ -1,10 +1,15 @@
 class Line:
 
 	def __init__(self,coor1,coor2):
-		pass
+		self.coor1 = coor1
+		self.coor2 = coor2
 
 	def distance(self):
-		pass
+		x1,y1 = self.coor1
+		x2,y2 = self.coor2
+		return (((x2 - x1)**2) + ((y2 - y1)**2))**0.5
 
 	def slope(self):
-		pass
+		x1,y1 = self.coor1
+		x2,y2 = self.coor2
+		return (y2-y1)/(x2-x1)
