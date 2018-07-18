8ca66b00616ad0ad8f856871caf60a22cf57b690
Tyler Gunter <tyler.james.gunter@gmail.com>
2018-07-18 08:50:37 -0400

    geometrics fromulas

--- OOP.py
+++ OOP.py
@@ -13,3 +13,30 @@ class Line:
 		x1,y1 = self.coor1
 		x2,y2 = self.coor2
 		return (y2-y1)/(x2-x1)
+
+coordinate1 = (3,2)
+coordinate2 = (8,10)
+
+li = Line(coordinate1,coordinate2)
+
+print(li.distance())
+print(li.slope())
+
+class Cylinder:
+	def __init__(self,height=1,radius=1):
+		self.height = height
+		self.radius = radius
+
+	def volume(self):
+		h = self.height
+		r = self.radius
+		return 3.14*(r**2)*h
+
+	def surface_area(self):
+		h = self.height
+		r = self.radius
+		return ((2 * 3.14)*r*h) + ((2* 3.1415)*(r**2))
+
+c = Cylinder(2,3)
+print(c.volume())
+print(c.surface_area())
