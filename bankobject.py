ebf3ae85050252c731b34885d0bb9fd6bad1f90d
Tyler Gunter <tyler.james.gunter@gmail.com>
2018-07-18 18:27:25 -0400

    completed bank account object

--- bankobject.py
+++ bankobject.py
@@ -4,9 +4,12 @@ class Account:
 		self.owner = owner
 		self.balance = balance
 
+	def __str__(self):
+		return f'Account owner: {self.owner}\nAccount balance: {self.balance}'
+
 	def deposit(self, dep_amt):
 		self.balance += dep_amt
-		print(f"Your deposit of {dep_amt} has been accepted")
+		print(f"Your deposit of ${dep_amt} has been accepted")
 
 	def withdraw(self,wd_amt):
 		if self.balance >= wd_amt:
@@ -15,4 +18,8 @@ class Account:
 
 		else:
 			print("You do not have sufficient funds to make the withdrawal you've requested.")
-		
+
+acct1 = Account('Tyler',100)
+print(acct1)
+#acct1.deposit(50)
+#acct1.withdraw(200)
